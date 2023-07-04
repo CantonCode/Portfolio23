4c0245af90ed43ee2462924c5a9e44716f18315f
Conor Canton <conorstreete@gmail.com>
2023-07-04 16:35:20 +0100

    added new job container and styling

--- chatbot/chatbotGPT/src/pages/home/home.tsx
+++ chatbot/chatbotGPT/src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import { AboutMe } from "../../components";
 import About from "../../components/About/About";
 import Intro from "../../components/Intro/Intro";
+import JobContainer from "../../components/Jobs/JobContainer/JobContainer";
 import ProjectCard from "../../components/Projects/ProjectCard/ProjectCard";
 import ProjectGrid from "../../components/Projects/ProjectGrid/ProjectGrid";
 import Tiles from "../../components/Tiles/Tiles";
@@ -13,6 +14,7 @@ function Home() {
         {/* <Intro/>
         <About/> */}
         <ProjectGrid/>
+        <JobContainer/>
       </div>
         
   
