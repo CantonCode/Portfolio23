c49b9699396c84bac9b398cab453e771b424d4ba
Conor Canton <conorstreete@gmail.com>
2023-07-06 14:35:01 +0100

    made job container dynamic

--- chatbot/chatbotGPT/src/components/Jobs/JobContainer/JobContainer.tsx
+++ chatbot/chatbotGPT/src/components/Jobs/JobContainer/JobContainer.tsx
@@ -1,68 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './JobContainer.css'
 import { Button, Card, Col, Container, Nav, Row, Tab, TabContainer, Tabs } from 'react-bootstrap'
+import { generateId } from '../../../helpers/generateIDHelper'
 
+const defaultVal = [
+    {
+      id: generateId(),
+      company: "JRI America",
+      role: "Junior Software Engineer",
+      date: "May 2022 - August 2023",
+      tasks: [ "Developed a web application using React, Redux, and Typescript to display data from a REST API",
+        "Created a REST API using Node.js and Express to serve data from a MySQL database",
+        "Implemented a CI/CD pipeline using Jenkins to automate the deployment of the web application"],
+
+    },
+    {
+      id: generateId(),
+      company: "Dairymaster",
+      role: "Graduate Software Engineer",
+      date: "May 2021 - May 2022",
+      tasks: [ "Developed a web application using React, Redux, and Typescript to display data from a REST API",
+        "Created a REST API using Node.js and Express to serve data from a MySQL database",
+        "Implemented a CI/CD pipeline using Jenkins to automate the deployment of the web application"],
+    },
+  ];
 
 
 // const ProjectCard = (props) =>  {
 function JobContainer(){
+    const [job, setJob] = useState(defaultVal);
+
   return (
     <Container >
-        <TabContainer defaultActiveKey="first" >
+        <TabContainer defaultActiveKey="0" >
             <Row xs={12} sm={12} md={8} style={{maxWidth:'100%',justifyContent:'center'}}>
                 <Col sm={{ span: 2, offset: 0 }}>
                     <Nav className="flex-md-column" style={{}}>
-                    <Nav.Item className='nav-items'>
-                        <Nav.Link className='nav-links' eventKey="first">JRI America</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='nav-items'>
-                        <Nav.Link className='nav-links' eventKey="second">Dairymaster</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='nav-items'>
-                        <Nav.Link className='nav-links' eventKey="third">WP Engine</Nav.Link>
-                    </Nav.Item>
+                        {job.map((job, k) => (
+                            <Nav.Item key={job.id} className='nav-items'>
+                                <Nav.Link className='nav-links' eventKey={k}>{job.company}</Nav.Link>
+                            </Nav.Item>
+                        ))
+                        }
                     </Nav>
                 </Col>
                 <Col sm={{ span: 6, offset: 0 }}>
                     <Tab.Content style={{color:'white'}}>
-                        <Tab.Pane eventKey="first">
-                            <Container>
-                                <Row style={{textAlign:'start'}}>
-                                    <Col xs={12}>
-                                    Software Engineer @ JRI America
-                                    </Col>
-                                    <Col xs={12}>
-                                    May 2022 - August 2023
-                                    </Col>
-                                    <Col xs={12}>
-                                    <ul>
-                                        <li>Developed a web application using React, Redux, and Typescript to display data from a REST API</li>
-                                        <li>Created a REST API using Node.js and Express to serve data from a MySQL database</li>
-                                        <li>Implemented a CI/CD pipeline using Jenkins to automate the deployment of the web application</li>
-                                    </ul>
-                                    </Col>
-                                </Row>
-                            </Container>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="second">
-                        <Container>
-                                <Row style={{textAlign:'start'}}>
-                                    <Col xs={12}>
-                                    Software Engineer @ Dairymaster
-                                    </Col>
-                                    <Col xs={12}>
-                                    April 2021 - April 2022
-                                    </Col>
-                                    <Col xs={12}>
-                                    <ul>
-                                        <li>Developed a web application using React, Redux, and Typescript to display data from a REST API</li>
-                                        <li>Created a REST API using Node.js and Express to serve data from a MySQL database</li>
-                                        <li>Implemented a CI/CD pipeline using Jenkins to automate the deployment of the web application</li>
-                                    </ul>
-                                    </Col>
-                                </Row>
-                            </Container>
-                        </Tab.Pane>
+                        {job.map((job, k) => (
+                            <Tab.Pane key={job.id} eventKey={k}>
+                                <Container>
+                                    <Row style={{textAlign:'start'}}>
+                                        <Col xs={12}>
+                                            {job.role}
+                                        </Col>
+                                        <Col xs={12}>
+                                            {job.date}
+                                        </Col>
+                                        <Col xs={12}>
+                                            <ul>
+                                                {job.tasks.map((task, z) => (
+                                                    <li key={z}>{task}</li>
+                                                ))}
+                                            </ul>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                                </Tab.Pane>
+                        ))}
                     </Tab.Content>
                 </Col>
                 
---
+++ chatbot/chatbotGPT/src/helpers/generateIDHelper.tsx
@@ -0,0 +1,6 @@
+const generateId = () => {
+    return (Math.random() + 1).toString(36).substring(8);
+  };
+  
+  export { generateId };
+  
