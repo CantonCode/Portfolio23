82f50d36e9ca570c72ef3e59094f3f2ed2d38349
Conor Canton <conorstreete@gmail.com>
2023-07-06 12:09:09 +0100

    style job container the way I wanted

--- chatbot/chatbotGPT/src/components/Jobs/JobContainer/JobContainer.tsx
+++ chatbot/chatbotGPT/src/components/Jobs/JobContainer/JobContainer.tsx
@@ -7,20 +7,23 @@ import { Button, Card, Col, Container, Nav, Row, Tab, TabContainer, Tabs } from 
 // const ProjectCard = (props) =>  {
 function JobContainer(){
   return (
-    <Container>
-        <TabContainer>
-            <Row>
-                <Col sm={3}>
-                    <Nav  className="flex-md-column" style={{}}>
+    <Container >
+        <TabContainer defaultActiveKey="first" >
+            <Row xs={12} sm={12} md={8} style={{maxWidth:'100%',justifyContent:'center'}}>
+                <Col sm={{ span: 2, offset: 0 }}>
+                    <Nav className="flex-md-column" style={{}}>
                     <Nav.Item className='nav-items'>
-                        <Nav.Link className='nav-link' eventKey="first">Tab 1</Nav.Link>
+                        <Nav.Link className='nav-links' eventKey="first">JRI America</Nav.Link>
                     </Nav.Item>
                     <Nav.Item className='nav-items'>
-                        <Nav.Link className='nav-link' eventKey="second">Tab 2</Nav.Link>
+                        <Nav.Link className='nav-links' eventKey="second">Dairymaster</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item className='nav-items'>
+                        <Nav.Link className='nav-links' eventKey="third">WP Engine</Nav.Link>
                     </Nav.Item>
                     </Nav>
                 </Col>
-                <Col sm={9}>
+                <Col sm={{ span: 6, offset: 0 }}>
                     <Tab.Content style={{color:'white'}}>
                         <Tab.Pane eventKey="first">First tab content</Tab.Pane>
                         <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
