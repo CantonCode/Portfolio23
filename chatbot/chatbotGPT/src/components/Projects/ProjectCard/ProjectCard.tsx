2202706b750866f56feb04d6f97eb0c0b6bb4b09
Conor Canton <conorstreete@gmail.com>
2023-07-11 17:15:42 +0100

    connect project card to modal dynamically

--- chatbot/chatbotGPT/src/components/Projects/ProjectCard/ProjectCard.tsx
+++ chatbot/chatbotGPT/src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import './ProejctCard.css'
 import { Button, Card, Container } from 'react-bootstrap'
+import ProjectModal from '../ProjectModal/ProjectModal';
 
 
 
 // const ProjectCard = (props) =>  {
 const ProjectCard = (project: any) => {
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <Container>
-        <Card onClick={() => console.log("MODAL CLICK")} style={{padding:'0rem',cursor: "pointer" }}>
+        <Card onClick={() => setModalShow(true)} style={{padding:'0rem',cursor: "pointer" }}>
             <Card.Img src="https://via.placeholder.com/150x75" />
 
             <Card.Body>
                 <Card.Title>{project.name}</Card.Title>
             </Card.Body>
         </Card>
+
+        <ProjectModal
+        {...project}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </Container> 
   )
 }
--- chatbot/chatbotGPT/src/components/Projects/ProjectModal/ProjectModal.tsx
+++ chatbot/chatbotGPT/src/components/Projects/ProjectModal/ProjectModal.tsx
@@ -12,7 +12,25 @@ const ProjectModal = (project: any) => {
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered
-        ></Modal>
+        >
+
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+                {project.name}
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <h4>Centered Modal</h4>
+            <p>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                consectetur ac, vestibulum at eros.
+            </p>
+            </Modal.Body>
+            <Modal.Footer>
+          
+            </Modal.Footer>
+        </Modal>
     </Container> 
   )
 }
