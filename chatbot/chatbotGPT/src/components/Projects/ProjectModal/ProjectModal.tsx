0cd3fbe3254b46be781df1eda017dcd8c5d6e1d3
Conor Canton <conorstreete@gmail.com>
2023-07-12 16:43:30 +0100

    added code to get dynamic technology tags

--- chatbot/chatbotGPT/src/components/Projects/ProjectModal/ProjectModal.tsx
+++ chatbot/chatbotGPT/src/components/Projects/ProjectModal/ProjectModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Badge, Button, Card, Col, Container, FloatingLabel, Modal, Row, Stack } from 'react-bootstrap'
 
-
+import { Reactjs,Node, } from 'grommet-icons'
 
 // const ProjectCard = (props) =>  {
 const ProjectModal = (project: any) => {
@@ -13,27 +13,32 @@ const ProjectModal = (project: any) => {
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
                 style={{textAlign:'center',justifyContent:'center',alignItems:'center'}}
+                scrollable={true}
                 >
-                <Row style={{justifyContent:'center'}}>
+                <Row style={{justifyContent:'center',display:'flex'}}>
                     <Col sm={8}>
-                    <Modal.Header style={{borderBottom:'0px'}} closeButton></Modal.Header>
-                    <Modal.Body>
-                    <h1>{project.name}</h1>
-                    <hr></hr>
+                        <Modal.Header style={{borderBottom:'0px'}} closeButton></Modal.Header>
+                        <Modal.Body style={{overflowY:'auto',maxHeight:'60vh'}}>
+                        <h1>{project.name}</h1>
+                        <hr></hr>
 
-                    <p style={{margin:'2rem'}}>
-                        {project.description}
-                    </p>
-                    </Modal.Body>
-                    <Modal.Footer style={{justifyContent:'center',display:'flex'}}>
-                            <Stack direction="horizontal" gap={2}>
-                                {project.technologies.map((tech: any) => (
-                                    <Badge pill bg="dark" text="white" style={{padding:'1rem'}}>
-                                        {tech}
-                                    </Badge>
-                                ))}
-                            </Stack>
-                    </Modal.Footer>
+                        <p style={{margin:'2rem'}}>
+                            {project.description}
+                        </p>
+                        </Modal.Body >
+                    </Col>
+                </Row>
+                <Row style={{justifyContent:'center'}}>
+                    <Col style={{height:'auto'}}>
+                        <Modal.Footer style={{justifyContent:'center',display:'flex',}}>
+                                <Stack direction="horizontal" gap={2} style={{display:'flex'}}>
+                                    {project.technologies.map((tech: any) => (
+                                        <Badge pill bg="dark" text="white" style={{padding:'1rem'}}>
+                                            {tech}
+                                        </Badge>
+                                    ))}
+                                </Stack>
+                        </Modal.Footer>
                     </Col>
                 </Row>
 
