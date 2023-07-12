4f26a07158ca50c6119936397e839d31a9c7ba05
Conor Canton <conorstreete@gmail.com>
2023-07-12 12:10:05 +0100

    added card overlay

--- chatbot/chatbotGPT/src/components/Projects/ProjectCard/ProjectCard.tsx
+++ chatbot/chatbotGPT/src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './ProejctCard.css'
 import { Button, Card, Container } from 'react-bootstrap'
 import ProjectModal from '../ProjectModal/ProjectModal';
+import { Add,Info } from 'grommet-icons';
+
 
 
 
@@ -9,13 +11,21 @@ import ProjectModal from '../ProjectModal/ProjectModal';
 const ProjectCard = (project: any) => {
   const [modalShow, setModalShow] = React.useState(false);
   return (
-    <Container>
-        <Card onClick={() => setModalShow(true)} style={{padding:'0rem',cursor: "pointer",height:'25vh',border:'2px solid grey'}}>
+    <Container >
+        <Card onClick={() => setModalShow(true)} style={{padding:'0rem',cursor: "pointer",height:'25vh',border:'2px solid grey',alignContent:'center',display:'flex'}}>
             <Card.Img src={project.image} style={{height:'80%',backgroundSize:'none'}}/>
 
             <Card.Body style={{borderTop:'2px solid black'}}>
                 <Card.Title>{project.name}</Card.Title>
             </Card.Body>
+
+            <div className="overlay" style={{justifyContent:'center',alignItems:'center',display:'flex'}}>
+              <div className='overlay-icon'>
+                <Info color='white' size='medium'/>
+                <div className="text" style={{color:'white'}}>View More</div>
+              </div>
+              
+            </div>
         </Card>
 
         <ProjectModal
