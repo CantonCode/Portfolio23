c3447b0b5a7c2d5f84f7550569b75846bc3fc8ec
Conor Canton <conorstreete@gmail.com>
2023-07-20 11:23:22 +0100

    added .env

--- app/backend/config.js
+++ app/backend/config.js
@@ -1,4 +1,8 @@
+var dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports ={
-    MONGO_URI: 'mongodb+srv://conorstreete:tj7EGcuAy2z2rFg1@cluster0.as5xkk1.mongodb.net/MyPortfolio'
+    MONGO_URI: 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD +'@cluster0.as5xkk1.mongodb.net/MyPortfolio'
 }
 //test
